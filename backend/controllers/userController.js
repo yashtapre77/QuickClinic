9e0758de0bf084f2f183311f0a8419aa6286f1cc
yashtapre77 <yashtapre77@gmail.com>
2025-07-10 06:57:48 +0530

    added doctor,appointment model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,30 +1,48 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
-  console.log("req.body:", req.body); 
-  const { name, email, password } = req.body;
-  const userExists = await User.findOne({ email });
-  if (userExists)
-    return res.status(400).json({ message: "User already exists" });
-
-  const user = await User.create({ name, email, password });
-  res.status(201).json({ id: user._id, email: user.email });
+  try{
+    console.log("req.body:", req.body); 
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+    
+    const user = await User.create({ name, email, password });
+    
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    
+    res.status(201).json({ id: user._id, email: user.email, token:token });
+  }
+  catch (error) {
+    console.error("Error in registerUser:", error);
+    res.json({ message: "Some error occured" });
+  }
 };
 
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) return res.status(400).json({ message: "Invalid credentials" });
+  try{
 
-  if (password !== user.password)
-    return res.status(400).json({ message: "Invalid credentials" });
-
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
-
-  res.json({ token });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+    
+    if (password !== user.password)
+      return res.status(400).json({ message: "Invalid credentials" });
+    
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    
+    res.json({ token });
+  }
+  catch (error) {
+    console.error("Error in loginUser:", error);
+    res.json({ message: "Some error occured" });
+  }
 };
 
 module.exports = { registerUser, loginUser };
--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,10 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  address: { type: Object, default: { line1: '', line2: '' } },
+  gender: { type: String, default: 'Not Selected' },
+  dob: { type: String, default: 'Not Selected' },
+  phone: { type: String, default: '00000000000', unique: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
